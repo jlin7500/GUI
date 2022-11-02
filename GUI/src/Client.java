9b55a3473e0235baf2ecf7eb7753467ec8f5f861
ChrisCaliendo <christiantcaliendo@gmail.com>
2022-11-02 14:06:43 -0400

    Create Client.java

---
+++ GUI/src/Client.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Client extends User{
+	
+	private ArrayList<Job> jobs;
+	
+	public Client(String newName, String newId, int newPhoneNumber) {
+		super(newName, newId, newPhoneNumber);
+	}
+	
+	
+	public void registerForNewJob() {
+		Job newJob = new Job();
+		newJob.addNewClient(name);
+		jobs.add(newJob);	
+	}
+	
+}
