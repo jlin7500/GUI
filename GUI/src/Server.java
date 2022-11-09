e1c26cd147ae74dcf66845527d3bc85c5bae21dd
ChrisCaliendo <christiantcaliendo@gmail.com>
2022-11-08 20:07:05 -0500

    Create Server.java

---
+++ GUI/src/Server.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+
+public class Server {
+	static ServerSocket serverSocket;
+    static Socket socket;
+    static DataInputStream inputStream;
+    static DataOutputStream outputStream;
+
+    public static void main(String[] args) {
+        String clientInput = "";
+        String serverOutput = "";
+        String response = "";
+        Scanner input;
+        boolean serverRunning = false;
+
+        try {
+            serverRunning = true;
+            System.out.println("Server Online");
+            System.out.println("Waiting for Response from Client");
+            serverSocket = new ServerSocket(3000);
+            socket = serverSocket.accept();
+            System.out.println("User Connected");
+
+            while (serverRunning) {
+                System.out.println("Waiting for Input");
+
+                inputStream = new DataInputStream(socket.getInputStream());
+                outputStream = new DataOutputStream(socket.getOutputStream());
+                
+                clientInput = inputStream.readUTF();
+                System.out.println("Input received");
+                System.out.println(clientInput.toString());
+                System.out.println("Would you like to save this message? y/n?");
+                input = new Scanner(System.in);
+                response = input.nextLine();
+                while(!response.equals("y") || !response.equals("n"))
+                {
+                	if (response.toLowerCase().equals("accept")) {
+                        System.out.println("Information accepted");
+                        serverOutput = "accept";
+                        break;
+                    }
+                    else if(response.toLowerCase().equals("reject"))
+                    {
+                        System.out.println("Information rejected");
+                        serverOutput = "reject";
+                        break;
+                    }
+                    else {
+                        System.out.println("That is not a valid response. Please try again.");
+                        response = input.nextLine();
+                    }
+                }
+                outputStream.writeUTF(serverOutput);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
