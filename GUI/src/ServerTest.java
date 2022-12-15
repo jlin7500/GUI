1bf9758323a6d3767525604d0923d7fc2ba58bf3
ChrisCaliendo <christiantcaliendo@gmail.com>
2022-12-14 19:50:09 -0500

    Final Update 2

--- GUI/src/ServerTest.java
+++ GUI/src/ServerTest.java
@@ -3,28 +3,25 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-
-
 public class ServerTest {
-	static ServerSocket serverSocket;
+    static ServerSocket serverSocket;
     static Socket socket;
     static DataInputStream inputStream;
     static DataOutputStream outputStream;
-    
+
     private static String clientNotification;
     private static int numOfMessagesSaved = 0;
-    
+
     public static void main(String[] args) {
         String clientInput = "";
         String serverOutput = "";
         String response = "";
         Scanner input;
-        
-        
+
         boolean serverRunning = false;
 
         try {
-        	clientNotification = " ";
+            clientNotification = " ";
             serverRunning = true;
             System.out.println("Server Online");
             System.out.println("Waiting for Response from Client");
@@ -37,94 +34,94 @@ public class ServerTest {
 
                 inputStream = new DataInputStream(socket.getInputStream());
                 outputStream = new DataOutputStream(socket.getOutputStream());
-                
+
                 clientInput = inputStream.readUTF();
                 System.out.println("Input received");
                 System.out.println(clientInput.toString());
                 String[] inputData = clientInput.split(";");
-                
+
                 String output = "Input received";
                 VehicalController vc = null;
                 vc = vc.accessController();
-                
-                switch(inputData[0]) {
-                
-                case "OwnerCarRegister":
-                	
-                  Car newCar = new Car(inputData[3], inputData[4], inputData[5], inputData[6], Integer.parseInt(inputData[7]));
-                  vc.addNewCar(newCar);
-                  
-                  break;
-                  
-                case "ClientNotifications":
-                	
-                	clientNotification="<html>"+clientNotification+"</html>";
-                    output = clientNotification;
-                    clientNotification = "";
-                    break;
-                  
-                case "ClientRequest":
-                	
-                  Job jobRequest = new Job();
-                  jobRequest.addNewClient(inputData[1], Integer.parseInt(inputData[2]));
-                  jobRequest.addNewDuration(Integer.parseInt(inputData[3]));
-                  jobRequest.addNewDeadline(Integer.parseInt(inputData[4]));
-                  
-                  vc.addNewJobRequest(jobRequest);
-                  
-                  break;
-                  
-                case "AdminHome":
-                    output = vc.getVCInfo();
-                    break;
-                    
-                case "AdminJobSize":
-                    output = String.valueOf(vc.getRequestListSize());
-                    break;
-                case "AdminCarSize":
-                    output = String.valueOf(vc.getCarListSize());
-                    break;
-                    
-                case "AdminRequests":
-                    output = vc.getAllJobRequests();
-                    break;
-                    
-                case "AdminCars":
-                    output = vc.getListOfAvailableCars();
-                    break;
-                    
-                case "AdminApprove":
-                	
-                	clientNotification = clientNotification + 
-                	"Your job request: "+
-                			vc.approveNewJob(Integer.parseInt(inputData[1])-1,Integer.parseInt(inputData[2])-1)
-                	+ "was approved";
-                	output = "Job has been Approved<br/>";
-                    break;
-                    
-                case "AdminDeny":
-                	clientNotification = clientNotification + 
-                	"Your job request: "+
-                		vc.denyNewJob(Integer.parseInt(inputData[1])-1)
-                	+"has been denied <br/>";
-                	output = "had been Denied";
-                    break;
-                default:
-                  // code block
-              }
-                
-                numOfMessagesSaved = numOfMessagesSaved  + 1;
+
+                switch (inputData[0]) {
+
+                    case "OwnerCarRegister":
+
+                        Car newCar = new Car(inputData[3], inputData[4], inputData[5], inputData[6],
+                                Integer.parseInt(inputData[7]));
+                        vc.addNewCar(newCar);
+
+                        break;
+
+                    case "ClientNotifications":
+
+                        clientNotification = "<html>" + clientNotification + "</html>";
+                        output = clientNotification;
+                        clientNotification = "";
+                        break;
+
+                    case "ClientRequest":
+
+                        Job jobRequest = new Job();
+                        jobRequest.addNewClient(inputData[1], Integer.parseInt(inputData[2]));
+                        jobRequest.addNewDuration(Integer.parseInt(inputData[3]));
+                        jobRequest.addNewDeadline(Integer.parseInt(inputData[4]));
+
+                        vc.addNewJobRequest(jobRequest);
+
+                        break;
+
+                    case "AdminHome":
+                        output = vc.getVCInfo();
+                        break;
+
+                    case "AdminJobSize":
+                        output = String.valueOf(vc.getRequestListSize());
+                        break;
+                    case "AdminCarSize":
+                        output = String.valueOf(vc.getCarListSize());
+                        break;
+
+                    case "AdminRequests":
+                        output = vc.getAllJobRequests();
+                        break;
+
+                    case "AdminCars":
+                        output = vc.getListOfAvailableCars();
+                        break;
+
+                    case "AdminApprove":
+
+                        clientNotification = clientNotification +
+                                "Your job request: " +
+                                vc.approveNewJob(Integer.parseInt(inputData[1]) - 1, Integer.parseInt(inputData[2]) - 1)
+                                + "was approved";
+                        output = "Job has been Approved<br/>";
+                        break;
+
+                    case "AdminDeny":
+                        clientNotification = clientNotification +
+                                "Your job request: " +
+                                vc.denyNewJob(Integer.parseInt(inputData[1]) - 1)
+                                + "has been denied <br/>";
+                        output = "had been Denied";
+                        break;
+                    default:
+                        // code block
+                }
+
+                numOfMessagesSaved = numOfMessagesSaved + 1;
                 getMessageSavedCount();
                 outputStream.writeUTF(output);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
-        
+
     }
-    
+
     public static void getMessageSavedCount() {
-    	System.out.println("Number of Messages Processed: "+numOfMessagesSaved);
+        System.out.println("Number of Messages Processed: " + numOfMessagesSaved);
     }
 }
--- GUI/src/VehicalController.java
+++ GUI/src/VehicalController.java
@@ -32,9 +32,10 @@ public class VehicalController {
 			sendToDatabase("DELETE FROM jobrequests");
 			sendToDatabase("DELETE FROM cars");
 			sendToDatabase("DELETE FROM jobs");
+			sendToDatabase("DELETE FROM deniedjobs");
 			CURRENT_MODEL = new VehicalController();
 		}
-			
+
 		return CURRENT_MODEL;
 	}
 
@@ -45,7 +46,8 @@ public class VehicalController {
 			output = "No Current Job Requests<br/>";
 		else {
 			for (int i = 0; requestList.size() > i; i++) {
-				output = output + "Request "+(i+1)+". "+requestList.get(i).getRequestAsString() + "<br/>";
+				output = output + "<b>Request " + (i + 1) + ". </b>" + requestList.get(i).getRequestAsString()
+						+ "<br/>";
 			}
 		}
 		return output;
@@ -59,22 +61,22 @@ public class VehicalController {
 			output = "No Cars Available<br/>";
 		else {
 			for (int i = 0; carList.size() > i; i++) {
-				
-				if(carList.get(i).status=="available") {
+
+				if (carList.get(i).status == "available") {
 					index = index + 1;
-					output = output + "Car "+index+". "+carList.get(i).getCarAsString() + "<br/>";
+					output = output + "<b>Car " + index + ". </b>" + carList.get(i).getCarAsString() + "<br/>";
 				}
 			}
 		}
 		return output;
 	}
-	
+
 	public static int getAvailableCarIndex(int index) {
 		ArrayList<Car> carList = CURRENT_MODEL.CAR_LIST;
-		for(int i = 0; index > i; i++) {
+		for (int i = 0; index > i; i++) {
 			Car currentCar = carList.get(i);
-			if(currentCar.status!="available") {
-				index = index+1;
+			if (currentCar.status != "available") {
+				index = index + 1;
 			}
 		}
 		return index;
@@ -87,7 +89,7 @@ public class VehicalController {
 			output = "No Current Ongoing Jobs<br/>";
 		else {
 			for (int i = 0; jobList.size() > i; i++) {
-				output = output + jobList.get(i).getJobAsString() + "<br/>";
+				output = output + "<b>Job " + (i + 1) + ". </b>" + jobList.get(i).getJobAsString() + "<br/>";
 			}
 		}
 		return output;
@@ -95,7 +97,8 @@ public class VehicalController {
 
 	public static String getVCInfo() {
 		return "<html>JOB REQUESTS: <br/>" + CURRENT_MODEL.getAllJobRequests() + "\nLIST OF AVAILABLE CARS: <br/>"
-				+ CURRENT_MODEL.getListOfAvailableCars() + "\nCURRENT JOBS: <br/>" + CURRENT_MODEL.getAllJobRequests()+"</html>";
+				+ CURRENT_MODEL.getListOfAvailableCars() + "\nCURRENT JOBS: <br/>" + CURRENT_MODEL.getAllCurrentJobs()
+				+ "</html>";
 	}
 
 	public static void addNewJobRequest(Job request) {
@@ -111,94 +114,96 @@ public class VehicalController {
 	}
 
 	public static String denyNewJob(int jobIndex) {
-		//System.out.print("not works");
+		// System.out.print("not works");
 		Job job = JOB_REQUESTS.remove(jobIndex);
-		//System.out.print("works");
+		// System.out.print("works");
 		job.changeStatus("denied");
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        LocalDateTime now = LocalDateTime.now();
+		LocalDateTime now = LocalDateTime.now();
 		String dateTimer = (dtf.format(now));
 		sendToDatabase("DELETE FROM jobrequests WHERE "
-                + "clientID='"+job.assignedClientId+"' AND name='"+job.assignedClient+"' "
-                + "AND jobDuration='"+job.duration+"' AND jobDeadline='"+job.deadline+"'");
+				+ "clientID='" + job.assignedClientId + "' AND name='" + job.assignedClient + "' "
+				+ "AND jobDuration='" + job.duration + "' AND jobDeadline='" + job.deadline + "'");
 		sendToDatabase("INSERT INTO deniedjobs"
-                + "(clientID , name , jobDuration , jobDeadline , denialTime)" + "VALUES ("
-                + job.assignedClientId + ", '" + job.assignedClient + "', '" + job.duration + "', '" + job.deadline + "', '"
-                + dateTimer + "')");
-		return job.getRequestAsString()+ "  at "+dateTimer;
+				+ "(clientID , name , jobDuration , jobDeadline , denialTime)" + "VALUES ("
+				+ job.assignedClientId + ", '" + job.assignedClient + "', '" + job.duration + "', '" + job.deadline
+				+ "', '"
+				+ dateTimer + "')");
+		return job.getRequestAsString() + "  at " + dateTimer;
 	}
 
 	public static String approveNewJob(int jobIndex, int carIndex) {
 		carIndex = getAvailableCarIndex(carIndex);
 		Job job = removeJobRequest(jobIndex);
-		Car car=CURRENT_MODEL.CAR_LIST.remove(carIndex);
+		Car car = CURRENT_MODEL.CAR_LIST.remove(carIndex);
 		job.changeStatus("ongoing");
 		car.changeStatus("in use");
 		job.assignCar(car);
 		sendToDatabase("DELETE FROM jobrequests WHERE "
-                + "clientID ='"+job.assignedClientId+"' AND name ='"+job.assignedClient+"' "
-                + "AND jobDuration ='"+job.duration+"' AND jobDeadline ='"+job.deadline+"'");
+				+ "clientID ='" + job.assignedClientId + "' AND name ='" + job.assignedClient + "' "
+				+ "AND jobDuration ='" + job.duration + "' AND jobDeadline ='" + job.deadline + "'");
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        LocalDateTime now = LocalDateTime.now();
+		LocalDateTime now = LocalDateTime.now();
 		String dateTimer = (dtf.format(now));
-		sendToDatabase("UPDATE cars	SET status = 'in use' WHERE carID = "+car.carId+";");
+		sendToDatabase("UPDATE cars	SET status = 'in use' WHERE carID = " + car.carId + ";");
 
 		sendToDatabase("INSERT INTO jobs"
-                + "(clientID , name , jobDuration , jobDeadline , carID, jobApprovalTime)" + "VALUES ("
-                + job.assignedClientId + ", '" + job.assignedClient + "', '" + job.duration + "', '" + job.deadline + "', '"
-                + car.carId + "', '" + dateTimer + "')");
+				+ "(clientID , name , jobDuration , jobDeadline , carID, jobApprovalTime)" + "VALUES ("
+				+ job.assignedClientId + ", '" + job.assignedClient + "', '" + job.duration + "', '" + job.deadline
+				+ "', '"
+				+ car.carId + "', '" + dateTimer + "')");
 		// job.addNewDeadline(newDeadline);
 		// job.addNewDuration(newDuration);
 		CURRENT_MODEL.CURRENT_JOBS.add(job);
-		//CURRENT_MODEL.CAR_LIST.add(car);
-		return job.getRequestAsString()+ "  at "+dateTimer;
-		
+		// CURRENT_MODEL.CAR_LIST.add(car);
+		return job.getRequestAsString() + "  at " + dateTimer;
+
 	}
-	
+
 	public static int getRequestListSize() {
 		return JOB_REQUESTS.size();
 	}
-	
+
 	public static int getCarListSize() {
-		
+
 		int size = CAR_LIST.size();
 		ArrayList<Car> carList = CURRENT_MODEL.CAR_LIST;
-		for(int i = 0; size > i; i++) {
+		for (int i = 0; size > i; i++) {
 			Car currentCar = carList.get(i);
-			if(currentCar.status!="available") {
-				size = size-1;
+			if (currentCar.status != "available") {
+				size = size - 1;
 			}
 		}
 		return size;
 	}
-	
+
 	public static int getJobListSize() {
 		return CURRENT_JOBS.size();
 	}
-	
+
 	public static void sendToDatabase(String sqlCommand) {
-        try {
-            // System.out.print("sql start");
-            // declares a connection to your database
-            connection = DriverManager.getConnection(url, username, password);
-            // creates an insert query
-            String sql = sqlCommand;
-            // establishes the connection session
-            Statement statement = connection.createStatement();
-            // executes the query
-            int row = statement.executeUpdate(sql);
-            // the return value is the indication of success or failure of the query
-            // execution
-            if (row > 0)
-                System.out.println("Data was inserted!");
-            // System.out.print("sql end");
-            connection.close();
+		try {
+			// System.out.print("sql start");
+			// declares a connection to your database
+			connection = DriverManager.getConnection(url, username, password);
+			// creates an insert query
+			String sql = sqlCommand;
+			// establishes the connection session
+			Statement statement = connection.createStatement();
+			// executes the query
+			int row = statement.executeUpdate(sql);
+			// the return value is the indication of success or failure of the query
+			// execution
+			if (row > 0)
+				System.out.println("Data was inserted!");
+			// System.out.print("sql end");
+			connection.close();
 
-        } catch (SQLException f) {
-            System.out.print("sql failure");
-            f.getMessage();
+		} catch (SQLException f) {
+			System.out.print("sql failure");
+			f.getMessage();
 
-        }
+		}
 
-    }
+	}
 }
